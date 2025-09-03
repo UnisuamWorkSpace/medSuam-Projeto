a8c26ea259475b84117c14e7cffafe83bc0763bc
Igor <silva.igor.014@gmail.com>
2025-09-03 18:08:00 -0300

    mudanças na pagina de login

---
+++ js/login.js
@@ -0,0 +1,37 @@
+function mostrarSenha (id, imagem)  {   
+
+    var inputType = $('#' + id)[0];
+    var eye = $('#' + imagem)[0];
+    console.log(eye.src);
+    if(inputType.type === "password") {
+        inputType.type = "text";
+        eye.src = "./images/eye.svg";
+    }else {
+       inputType.type = "password"; 
+       eye.src = "./images/eye-slash.svg";
+    }
+
+}
+
+const form = $('#loginForm').on("submit", (event) => {
+
+
+    
+    event.preventDefault();
+
+    
+    const btn = $('#enviarBtn');
+        
+        btn.addClass("enviarClick")
+        
+        $(".enviarClick").css({
+               
+            });
+
+        setTimeout ( () => {
+            btn.removeClass("enviarClick");
+
+        }, 500); 
+    
+});
+
