4cb289894082b726bd539195cfec50f4c3e1c0df
Igor <silva.igor.014@gmail.com>
2025-09-10 16:50:07 -0300

    adição do botão de acessibilidade na pagina inicial

--- js/index.js
+++ js/index.js
@@ -9,3 +9,34 @@ $("#menu").on("click", () => {
 
     }
 }); 
+
+  let baseFontSize = 25;   
+
+  let maxIncrease = 5;     
+
+  let currentIncrease = 0; 
+
+function increaseFont() {
+  if (currentIncrease < maxIncrease) {
+    currentIncrease++;
+    document.body.style.fontSize = (baseFontSize + currentIncrease) + "px";
+    $("span:not(.h1Span)").css("font-size", (baseFontSize + currentIncrease) + "px");
+    $("p").css("font-size", (baseFontSize + currentIncrease) + "px");
+    $("a").css("font-size", (baseFontSize + currentIncrease) + "px");
+    
+
+  
+  }
+}
+
+function decreaseFont() {
+  if (currentIncrease > -5) {
+    currentIncrease--;
+    document.body.style.fontSize = (baseFontSize + currentIncrease) + "px";
+    $("span:not(.h1Span)").css("font-size", (baseFontSize + currentIncrease) + "px");
+    $("p").css("font-size", (baseFontSize + currentIncrease) + "px");
+    $("a").css("font-size", (baseFontSize + currentIncrease) + "px");
+
+
+  }
+}
