67c776394e5bada99a63855063c95c19f0f29e21
Ronivaldo Domingues de Andrade <ronidomingues.ard@gmail.com>
2025-10-27 16:18:20 -0300

    Update favicon links in index and login pages for consistency

--- admin/login.php
+++ admin/login.php
@@ -21,7 +21,7 @@ if (isset($_SESSION['admin_logged_in']) && $_SESSION['admin_logged_in'] === true
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login - MedSuam Admin</title>
-    <link rel="icon" type="image/x-icon" href="images/favicon_medsuam.png" type="image/png">
+    <link rel="icon" type="image/x-icon" href="images/logo_medsuam.png" type="image/png">
     <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
 </head>
