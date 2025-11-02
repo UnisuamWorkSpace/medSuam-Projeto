42e48bb577348bb9098289d758eb7b7739d2ae97
Ronivaldo Domingues de Andrade <ronidomingues.ard@gmail.com>
2025-11-02 13:35:53 -0300

    Fix stylesheet link for 2FA verification page

--- admin/2fa.php
+++ admin/2fa.php
@@ -65,7 +65,7 @@ if (!isset($_SESSION['2fa_code_sent']) && isset($_SESSION['admin_email'])) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Verificação em Duas Etapas - MedSuam</title>
     <link rel="icon" type="image/x-icon" href="images/logo_medsuam.png">
-    <link rel="stylesheet" href="css/twofa.css">
+    <link rel="stylesheet" href="css/2fa.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
 </head>
 <body>
